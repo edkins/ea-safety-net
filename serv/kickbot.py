4e7b97bcb6abf26e5e37b21eb1cc03153ec373bb
Giles Edkins <edkins@gmail.com>
2017-04-16 20:33:14 -0400

    Kickbot kicks users when they say "kick me"

--- serv/kickbot.py
+++ serv/kickbot.py
@@ -2,6 +2,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 import jsonschema
 import hashlib
+import requests
 
 kickbot_conf_schema = json.load(open('/opt/easn/json-schema/kickbot-conf.json'))
 slack_request_schema = json.load(open('/opt/easn/json-schema/slack-request.json'))
@@ -14,6 +15,17 @@ def read_json_file(filename, schema):
 		return obj
 
 kickbot_conf = read_json_file('/etc/easn/kickbot.conf', kickbot_conf_schema)
+
+def slack_api(method, data):
+	data = data.copy()
+	data['token'] = kickbot_conf['oauthToken']
+	url = 'https://slack.com/api/'+method
+	r = requests.post(url, data=data)
+	r.raise_for_status()
+	response = r.json()
+	if 'ok' not in response or not response['ok']:
+		raise ValueError('Non-ok response for slack api method ' + method)
+	return response
 
 class KickBotHandler(BaseHTTPRequestHandler):
 	def read_json(self,schema):
@@ -38,28 +50,41 @@ class KickBotHandler(BaseHTTPRequestHandler):
 		self.end_headers()
 		self.wfile.write(bytes(json_data,'utf-8'))
 
+	def empty_response(self):
+		self.send_response(200)
+		self.end_headers()
+
 	def do_GET(self):
-		#self.send_response(200)
-		#self.end_headers()
-		#self.wfile.write(bytes('Hello and stuff','utf-8'))
 		self.send_error(404)
 
 	def do_POST(self):
 		if self.path == '/kickbot/slackendpoint':
 			req = self.read_json(slack_request_schema)
+			self.check_slack_token(req['token'])
 			if req['type'] == 'url_verification':
-				self.slack_verification(req)
+				self.slack_verification_response(req)
+			elif req['type'] == 'event_callback':
+				self.slack_event_callback(req)
 			else:
 				self.send_error(500)
 		else:
 			self.send_error(404)
 
-	def slack_verification(self, req):
-		tokenhash = hashlib.sha256(bytes(req['token'],'ascii')).hexdigest()
+	def check_slack_token(self, token):
+		tokenhash = hashlib.sha256(bytes(token,'ascii')).hexdigest()
 		if tokenhash != kickbot_conf['slackTokenHash']:
 			self.send_error(400)
 			raise ValueError('Incorrect slack token')
+
+	def slack_verification_response(self, req):
 		self.json(slack_challenge_response_schema, {"challenge":req['challenge']})
+
+	def slack_event_callback(self, req):
+		ev = req['event']
+		if 'text' in ev and 'kick me' in ev['text'] and 'channel' in ev and 'user' in ev:
+			print('Kicking a user')
+			slack_api('channels.kick', {"channel":ev['channel'], "user":ev['user']})
+		self.empty_response()
 
 def run():
 	server_address = ('127.0.0.1', 8080)
